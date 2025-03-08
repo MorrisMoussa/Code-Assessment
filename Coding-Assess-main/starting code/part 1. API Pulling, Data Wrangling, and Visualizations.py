5d4e783afde34fcd211cd345fc7c2c3b108b98fb
MorrisMoussa <morris.m.moussa@gmail.com>
2025-03-07 17:13:14 -0800

    Update part 1. API Pulling, Data Wrangling, and Visualizations.py
    stable v1

--- Coding-Assess-main/starting code/part 1. API Pulling, Data Wrangling, and Visualizations.py
+++ Coding-Assess-main/starting code/part 1. API Pulling, Data Wrangling, and Visualizations.py
@@ -20,6 +20,42 @@ from scipy import optimize, stats
 #imprt sklearn as sp
 from sklearn import linear_model
 import statsmodels.api as sm
+import os
+
+wd = os.getcwd()
+data_directory = str(wd) + "\\Coding-Assess-main\\data\\"
+input_file = "Part 1. bonds_yields.xlsx"
+df_input pd.read_excel(data_directory + input_file)
 
 
-fred = Fred(api_key='60af7aa584e88ed55ad85bad09c2c9d7')
+def fetch_fred_yield(series_id, start_date, end_date, api_key):
+   
+    df = pd.DataFrame(fred.get_series(series_id, observation_start=start_date, observation_end= end_date, frequency = 'd')
+                      , columns= [series_id])
+    df.index.name = 'date'
+    return df  
+
+#my API key
+my_api_key = "60af7aa584e88ed55ad85bad09c2c9d7"
+# Initialize API key
+fred = Fred(api_key=my_api_key)
+
+#date range
+start_date = "2023-01-01"
+end_date="2023-12-31"
+# List of Treasury yield series IDs on FRED
+tenor_series_ids = [
+    "DGS1MO", "DGS3MO", "DGS6MO", "DGS1",  # Short-term yields
+    "DGS2", "DGS3", "DGS5",               # Medium-term yields
+    "DGS7", "DGS10", "DGS20", "DGS30"     # Long-term yields
+]
+
+df1 = fetch_fred_yield(tenor_series_ids[0], start_date, end_date, my_api_key)
+
+
+
+
+
+
+
+
